274e8b37a9bce9da905ebb61cfefc238d145e5d3
Levi Everitt <7173714-lhideki169@users.noreply.replit.com>
2025-09-15 20:41:14 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@workspace/.ccls-cache@@home@runner@CPlusPlus@main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char first, middle, last;
+  //store your initials in the variables
+  first = 'S';
+  middle = 'K';
+  last = 'S';
+
+  cout << first << middle << last;
+  return 0;
+}
+  
+  
+  
+  
