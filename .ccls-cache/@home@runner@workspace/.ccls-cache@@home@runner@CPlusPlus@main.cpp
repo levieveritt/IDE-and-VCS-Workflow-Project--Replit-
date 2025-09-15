d2baa46c93627355c5290051365bdb97f13b24e3
levieveritt <lhideki169@gmail.com>
2025-09-15 21:10:19 +0000

    Changes the initials in my code

--- .ccls-cache/@home@runner@workspace/.ccls-cache@@home@runner@CPlusPlus@main.cpp
+++ .ccls-cache/@home@runner@workspace/.ccls-cache@@home@runner@CPlusPlus@main.cpp
@@ -6,9 +6,9 @@ int main()
 {
   char first, middle, last;
   //store your initials in the variables
-  first = 'S';
-  middle = 'K';
-  last = 'S';
+  first = 'L';
+  middle = 'H';
+  last = 'E';
 
   cout << first << middle << last;
   return 0;
