ea50378596e76d21cc53a2e8bff69844ab3e1fc9
levieveritt <lhideki169@gmail.com>
2025-12-02 00:10:53 +0000

    changed to rectangle properties

--- main.cpp
+++ main.cpp
@@ -2,14 +2,43 @@
 
 using namespace std;
 
-int main()
-{
-  char first, middle, last;
-  //store your information in the variables
-  first = 'L';
-  middle = 'H';
-  last = 'E';
+// Function to calculate the perimeter of a rectangle
+double calculatePerimeter(double length, double width) {
+    return 2 * (length + width);
+}
 
-  cout << first << middle << last <<endl;
-  return 0;
+// Function to calculate the area of a rectangle
+double calculateArea(double length, double width) {
+    return length * width;
+}
+
+int main() {
+    double length, width;
+    char continueCalculation;
+
+    do {
+        // Get input from the user
+        cout << "Enter the length of the rectangle: ";
+        cin >> length;
+
+        cout << "Enter the width of the rectangle: ";
+        cin >> width;
+
+        // Calculate the perimeter and area
+        double perimeter = calculatePerimeter(length, width);
+        double area = calculateArea(length, width);
+
+        // Display the results
+        cout << "Perimeter: " << perimeter << endl;
+        cout << "Area: " << area << endl;
+
+        // Ask if the user wants to process another rectangle
+        cout << "Do you want to process another rectangle? (y/n): ";
+        cin >> continueCalculation;
+
+    } while (continueCalculation == 'y' || continueCalculation == 'Y');
+
+    cout << "Program terminated." << endl;
+
+    return 0;
 }
