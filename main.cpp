bb4bdcc9355087d6ff8117335088446ee6530652
levieveritt <lhideki169@gmail.com>
2025-12-02 01:32:20 +0000

    paint job

--- main.cpp
+++ main.cpp
@@ -1,64 +1,128 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-
-double getScore() {
-    double score;
-    do {
-        cout << "Enter a score between 0 and 10: ";
-        cin >> score;
-        if (score < 0 || score > 10) {
-            cout << "Invalid score. Please enter a score between 0 and 10." << endl;
-        }
-    } while (score < 0 || score > 10);
-    return score;
-}
-
-
-double findLowest(const vector<double>& scores) {
-    return *min_element(scores.begin(), scores.end());
-}
-
-
-double findHighest(const vector<double>& scores) {
-    return *max_element(scores.begin(), scores.end());
-}
-
-double calculateAverage(vector<double> scores) {
-    if (scores.size() < 3) return 0;
-
-    double lowest = findLowest(scores);
-    double highest = findHighest(scores);
-
-    scores.erase(remove(scores.begin(), scores.end(), lowest), scores.end());
-    if(lowest == highest){
-        scores.erase(remove(scores.begin(), scores.end(), highest), scores.end());
-    } else {
-         scores.erase(remove(scores.begin(), scores.end(), highest), scores.end());
-    }
-
-
-    double sum = accumulate(scores.begin(), scores.end(), 0.0);
-    return sum / scores.size();
-}
+// Function prototypes
+int getNumberOfRooms();
+double getSquareFeet(int roomNumber);
+double getPaintPrice();
+int calculateGallons(double squareFeet);
+double calculateLaborHours(double squareFeet);
+double calculatePaintCost(int gallons, double paintPrice);
+double calculateLaborCost(double laborHours);
+void displayEstimate(int gallons, double laborHours, double paintCost, double laborCost, double totalCost);
 
 int main() {
-    cout << "This program calculates a performer's score." << endl;
-    cout << "Five judges will each contribute a score, the highest and lowest scores will be dropped, and the remaining three will be averaged." << endl;
+    int numRooms;
+    double totalSquareFeet = 0.0;
+    double paintPrice;
+    int totalGallons = 0;
+    double totalLaborHours = 0.0;
+    double totalPaintCost = 0.0;
+    double totalLaborCost = 0.0;
+    double totalCost = 0.0;
 
-    vector<double> scores;
-    for (int i = 1; i <= 5; ++i) {
-        cout << "Score from judge " << i << ": ";
-        scores.push_back(getScore());
+    // # of rooms
+    numRooms = getNumberOfRooms();
+
+    // paint price
+    paintPrice = getPaintPrice();
+
+    // rooms
+    for (int i = 1; i <= numRooms; i++) {
+        double squareFeet = getSquareFeet(i);
+        totalSquareFeet += squareFeet;
+
+        int gallons = calculateGallons(squareFeet);
+        totalGallons += gallons;
+
+        double laborHours = calculateLaborHours(squareFeet);
+        totalLaborHours += laborHours;
+
+        double paintCost = calculatePaintCost(gallons, paintPrice);
+        totalPaintCost += paintCost;
+
+        double laborCost = calculateLaborCost(laborHours);
+        totalLaborCost += laborCost;
     }
 
-    double averageScore = calculateAverage(scores);
+    // total cost
+    totalCost = totalPaintCost + totalLaborCost;
 
-    cout << "The performer's final score is: " << averageScore << endl;
+    // estimate
+    displayEstimate(totalGallons, totalLaborHours, totalPaintCost, totalLaborCost, totalCost);
 
     return 0;
+}
+
+// # of rooms
+int getNumberOfRooms() {
+    int numRooms;
+    do {
+        cout << "Enter the number of rooms to be painted: ";
+        cin >> numRooms;
+        if (numRooms < 1) {
+            cout << "Number of rooms must be at least 1. Please enter again." << endl;
+        }
+    } while (numRooms < 1);
+    return numRooms;
+}
+
+// sq ft of wall
+double getSquareFeet(int roomNumber) {
+    double squareFeet;
+    do {
+        cout << "Enter the square footage of room " << roomNumber << ": ";
+        cin >> squareFeet;
+        if (squareFeet < 0) {
+            cout << "Square footage cannot be negative. Please enter again." << endl;
+        }
+    } while (squareFeet < 0);
+    return squareFeet;
+}
+
+// price of paint per gallon
+double getPaintPrice() {
+    double paintPrice;
+    do {
+        cout << "Enter the price of paint per gallon: $";
+        cin >> paintPrice;
+        if (paintPrice < 10.00) {
+            cout << "Price of paint must be at least $10.00. Please enter again." << endl;
+        }
+    } while (paintPrice < 10.00);
+    return paintPrice;
+}
+
+// # of gallons of paint required
+int calculateGallons(double squareFeet) {
+    return ceil(squareFeet / 110.0);
+}
+
+// # of labor hours
+double calculateLaborHours(double squareFeet) {
+    return (squareFeet / 110.0) * 8.0;
+}
+
+// cost of paint
+double calculatePaintCost(int gallons, double paintPrice) {
+    return gallons * paintPrice;
+}
+
+// cost of labor
+double calculateLaborCost(double laborHours) {
+    return laborHours * 25.0;
+}
+
+// estimate
+void displayEstimate(int gallons, double laborHours, double paintCost, double laborCost, double totalCost) {
+    cout << fixed << setprecision(2);
+    cout << "\nEstimate:" << endl;
+    cout << "Number of gallons of paint required: " << gallons << endl;
+    cout << "Hours of labor required: " << laborHours << endl;
+    cout << "Cost of the paint: $" << paintCost << endl;
+    cout << "Labor charges: $" << laborCost << endl;
+    cout << "Total cost of the paint job: $" << totalCost << endl;
 }
